5dbc7a44ca6ecce237b7560fa1cd560681346dba
janderson77 <jamesseventyseven@gmail.com>
2020-05-16 21:42:14 -0600

    Fixed Everything

--- meme.js
+++ meme.js
@@ -1,20 +1,3 @@
-flexFont = function() {
-	var divs = document.getElementsByClassName('meme');
-	for (var i = 0; i < divs.length; i++) {
-		var relFontsize = divs[i].offsetWidth * 0.05;
-		divs[i].style.fontSize = relFontsize + 'px';
-	}
-};
-
-window.onload = function(event) {
-	flexFont();
-};
-window.onresize = function(event) {
-	flexFont();
-};
-
-//I found the flexFont function from https://codepen.io/aknip/pen/GpGevp
-
 const form = document.querySelector('form');
 const imgInput = document.querySelector('#memeInput');
 const output = document.querySelector('#output');
@@ -34,12 +17,10 @@ form.addEventListener('submit', function(e) {
 	let topTextBox = document.createElement('div');
 	topTextBox.classList.add('topText');
 	let topTextText = document.createElement('p');
-	topTextText.innerText = topTextInput.value;
 
 	let bottomTextBox = document.createElement('div');
 	bottomTextBox.classList.add('bottomText');
 	let bottomTextText = document.createElement('p');
-	bottomTextText.innerText = bottomTextInput.value;
 
 	let deleteButton = document.createElement('button');
 	deleteButton.classList.add('delete');
@@ -53,11 +34,16 @@ form.addEventListener('submit', function(e) {
 	bottomTextBox.appendChild(bottomTextText);
 	memeWrap.appendChild(deleteButton);
 
+	topTextText.innerText = topTextInput.value;
+	bottomTextText.innerText = bottomTextInput.value;
+
 	imgInput.value = '';
 	topTextInput.value = '';
 	bottomTextInput.value = '';
 
-	flexFont();
+	let relFontsize = memeWrap.offsetWidth * 0.05;
+	topTextBox.style.fontSize = relFontsize + 'px';
+	bottomTextText.style.fontSize = relFontsize + 'px';
 });
 
 output.addEventListener('click', function(e) {
